d2f8dbdeae69609dc06b9dfe3fb6f8e20b5896a5
MatviyVovchuk <doc25900@gmail.com>
2024-07-05 09:31:06 +0300

    Testing pre-commit hook with ESLint

---
+++ js/test-no-errors.js
@@ -0,0 +1,7 @@
+// This file has no syntax or linting errors
+
+function testFunction() {
+    console.log("This is a test function");
+}
+
+testFunction();
---
+++ js/test-with-errors.js
@@ -0,0 +1,7 @@
+// This file has a syntax error and a linting error
+
+function testFunction() {
+    console.log("This is a test function");
+}
+
+testFunction()
